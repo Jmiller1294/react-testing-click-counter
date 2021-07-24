0c72cfa4a7ffadbe2957c170394c3ca033f6aab4
Justin Miller <jayman1294@gmail.com>
2021-07-24 19:32:16 -0400

    created helper functions in app test file

--- src/App.test.js
+++ src/App.test.js
@@ -4,19 +4,30 @@ import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+//function to create a shallow wrapper fro App component
+const setup = () => shallow(<App />);
+
+const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`);
+
 test('renders without error', () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='component-app']");
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, "component-app")
   expect(appComponent.length).toBe(1);
 });
 
 test('renders increment button', () => {
-
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, 'increment-button')
+  expect(button.length).toBe(1);
 });
 
 test('renders counter display', () => {
-
+  const wrapper = setup();
+  const counter = findByTestAttr(wrapper, 'counter-display')
+  expect(counter.length).toBe(1);
 });
+
+
 
 test('counter starts at 0', () => {
 
